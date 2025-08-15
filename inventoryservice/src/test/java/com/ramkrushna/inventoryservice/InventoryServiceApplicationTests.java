821a76d1ef41d62314fff04bf920719f87f41e94
ramkrishna500 <madoleramkrushna2@gmail.com>
2025-08-16 01:00:58 +0530

    Renamed inventory-service folder to inventoryservice and fixed pom/test issues

--- inventoryservice/src/test/java/com/ramkrushna/inventoryservice/InventoryServiceApplicationTests.java
+++ inventoryservice/src/test/java/com/ramkrushna/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ramkrushna.inventory_service;
+package com.ramkrushna.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
